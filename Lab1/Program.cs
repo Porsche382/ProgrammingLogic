60a91073560c0971581ba0001afeaf8292bcb627
Porsche382 <62913625+Porsche382@users.noreply.github.com>
2025-03-25 01:59:36 +0000

    Modified 1st Lab

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace Lab1;
+namespace Lab1;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+         //Step 1
+        Console.WriteLine("Porsha Stewart IT-1050");
+    // Step 2
+        /*Porsha IT-1050 - Lab1*/
+        //Step 3
+    string lingo = "Java"; 
+        Console.WriteLine("My favorite programming language is" + lingo);
+    int num = 6; 
+        Console.WriteLine("My favorite number is" + num);
+    double digit = 0; 
+        Console.WriteLine("How many programs I wrote before this class?" + digit);
+    bool statement = true; 
+        Console.WriteLine("I do not have programing experience" + statement);
+        //Step 5
+    const string school = "Tri-C";
+        Console.WriteLine(school);
     }
 }
