da6e3b92a5db6f98cec96edae990bded63db4ba7
Porsche382 <62913625+Porsche382@users.noreply.github.com>
2025-04-07 20:12:47 +0000

    Mod

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be79db71d8e48668c918bfb89bab2902fb8de01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db1f3dc6077c8d75b51af1358497b2ecf587cce9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
