123607b61b4eaad993bbc744a519cf2fcb5594f0
Porsche382 <62913625+Porsche382@users.noreply.github.com>
2025-04-22 14:40:33 +0000

    Modified

--- Lab5/obj/Debug/net8.0/Lab5.AssemblyInfo.cs
+++ Lab5/obj/Debug/net8.0/Lab5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e849672b19148a8ec59f35610677c56efe2466")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad25e856ae8ca71125b472890d417535466aba14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
